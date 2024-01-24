4fb6107da542d0f4b4ee258e59e0244e48d4f57f
Vikram Naik <26vikramnaik@gmail.com>
2024-01-24 23:22:19 +0530

    login and register page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,48 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Navigate,
+  Route,
+} from 'react-router-dom'
+import Home from './pages/home/Home'
 import Login from './pages/login/Login'
+import Regiter from './pages/register/Regiter'
+import Profile from './pages/profile/Profile'
 
 const App = () => {
+  const router = createBrowserRouter([
+    /* {
+      path: '/',
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ), 
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/profile/:id',
+          element: <Profile />,
+        },
+      ],
+    }, */
+    {
+      path: '/login',
+      element: <Login />,
+    },
+    {
+      path: '/register',
+      element: <Regiter />,
+    },
+  ])
+
   return (
     <div>
-      <Login />
+      <RouterProvider router={router} />
     </div>
   )
 }
